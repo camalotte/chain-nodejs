6a0d2a620e9794274e5dcc04e7a49d7683c298e7
Kerim Campbell <kerim.me@icloud.com>
2023-03-30 15:11:56 +0100

    login with memory SQLlite working

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
+const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 
@@ -10,18 +11,67 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-const users = new Map();
+// const users = new Map();
+
+const db = new sqlite3.Database(":memory:", (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log("Connected to the in-memory SQLite database.");
+});
+db.run(
+    `CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  )`,
+    (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+    }
+);
+
 const loginUser = async (username, password) => {
-    if (!users.has(username)) {
-        return null;
-    }
-    const hashedPassword = users.get(username);
-    const passwordMatch = await bcrypt.compare(password, hashedPassword);
-    if (!passwordMatch) {
-        return null;
-    }
-    return { username, password: hashedPassword };
+    return new Promise((resolve, reject) => {
+        db.get("SELECT * FROM users WHERE username = ?", [username], async (err, row) => {
+            if (err) {
+                reject(err);
+            }
+            if (!row) {
+                resolve(null);
+            } else {
+                const passwordMatch = await bcrypt.compare(password, row.password);
+                if (!passwordMatch) {
+                    resolve(null);
+                } else {
+                    resolve({ username: row.username, password: row.password });
+                }
+            }
+        });
+    });
 };
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        res.status(400).json({ message: "Missing username or password" });
+        return;
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    db.run(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        [username, hashedPassword],
+        (err) => {
+            if (err) {
+                res.status(409).json({ message: "Username already exists" });
+                return;
+            }
+            console.log("Registered user:", username);
+            res.status(201).json({ message: "User registered successfully" });
+        }
+    );
+});
 
 app.post("/login", async (req, res) => {
     try {
@@ -37,22 +87,6 @@ app.post("/login", async (req, res) => {
     }
 });
 
-
-app.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-        res.status(400).json({ message: "Missing username or password" });
-        return;
-    }
-    if (users.has(username)) {
-        res.status(409).json({ message: "Username already exists" });
-        return;
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    users.set(username, hashedPassword);
-    console.log("Registered user:", username);
-    res.status(201).json({ message: "User registered successfully" });
-});
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
